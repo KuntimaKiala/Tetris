f1a200a66682bbbb9b7d51f530ea1189a7c9aedf
KuntimaKiala <Kuntima.kiala@gmail.com>
2023-09-24 05:58:48 +0200

    Rotation Function to give the index of individual elements in the tetronimo

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,25 @@ using std::string ;
 
 
 std::wstring tetromino[7] ;
+int nFieldWidth = 12 ;
+int nFildHeight =18 ;
+
+// base on the rotation of tetromino returns the index of particular position {of pixel}
+int Rotate(int px, int py, int r, int w=4){
+    // r is 1, or, 2, or 3, or 4
+    switch (r%4)
+    {
+    case 0: return py*w+px;  //0 degrees rotation
+    case 1: return 12 + py -px*w ; //90 degrees rotation
+    case 2: return 15 -py*4-px;  //180 degrees rotation
+    case 3: return 3 -py +px*w ; //270 degress rotation
+    default : return 0 ;
+    }
+
+   
+}
+
+
 int main()
 {
     // initializes the ncurses library and sets up the terminal to be used with ncurses.
